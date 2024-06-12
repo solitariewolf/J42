57b99f38abd0a12d445589d8885e301063ca921c
solitariewolf <watanasio@gmail.com>
2024-06-12 01:15:26 +0000

    modified:   .gitignore 	modified:   commands/__pycache__/times.cpython-310.pyc 	modified:   commands/times.py

--- commands/times.py
+++ commands/times.py
@@ -12,17 +12,28 @@ class Times(commands.Cog):
             with open('user_times.txt', 'r') as f:
                 lines = f.readlines()
             if lines:
-                response = "Tempo total dos usuários nos canais de voz (HH:mm:ss):\n"
+                user_times = []
                 for line in lines:
                     try:
                         name, time_str = line.strip().rsplit(' ', 1)
                         total_duration = timedelta(seconds=float(time_str))
-                        hours, remainder = divmod(total_duration.total_seconds(), 3600)
-                        minutes, seconds = divmod(remainder, 60)
-                        formatted_duration = f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
-                        response += f'{name} passou um total de {formatted_duration} no canal de voz.\n'
+                        user_times.append((name, total_duration))
                     except ValueError:
-                        response += f"Linha malformada: {line}\n"
+                        user_times.append((f"Linha malformada: {line}", timedelta(0)))
+
+                # Ordena a lista pelo tempo total decrescente
+                user_times.sort(key=lambda x: x[1], reverse=True)
+
+                # Cria a resposta formatada
+                response = "**Tempo total dos usuários nos canais de voz (HH:mm:ss):**\n\n"
+                medals = ["🥇", "🥈", "🥉"]
+                for index, (name, total_duration) in enumerate(user_times):
+                    hours, remainder = divmod(total_duration.total_seconds(), 3600)
+                    minutes, seconds = divmod(remainder, 60)
+                    formatted_duration = f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
+                    medal = medals[index] if index < 3 else ""
+                    response += f"{medal} **{name}** passou um total de **{formatted_duration}** no canal de voz.\n"
+
                 await ctx.send(response)
             else:
                 await ctx.send("Nenhum registro encontrado.")
